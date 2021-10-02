ffccb72830556aee0fcf154f5d1a0b3a4ac7d44f
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-02 22:24:59 +0530

    Modified files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  color: white;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
+      <Header> React Movie App </Header>
       Kiranpal Singh
-    </div>
+    </Container>
   );
 }
 
