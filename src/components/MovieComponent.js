7c3ca21889590ebb90a51b199efe6bf8e27f6307
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-04 20:26:04 +0530

    Styled movie component with image and movie name

--- src/components/MovieComponent.js
+++ src/components/MovieComponent.js
@@ -1,6 +1,37 @@
+import styled from "styled-components";
+
+const MovieContainer = styled.div`
+ display: flex;
+ flex-direction: column;
+ padding: 10px;
+ width: 280px;
+ box-shadow: 0 3px 10px 0 #aaa;
+ cursor: pointer;
+`;
+
+const CoverImage = styled.img`
+    object-fit: cover;
+    height: 362px;
+`
+
+const MovieName = styled.span` 
+    font-size : 18px;
+    font-weight: 600;
+    margin: 15px 0;
+    color: black;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+`;
+
 const MovieComponent = (props) => {
     
-    return (<div> Movie  </div>)
-}
+    return (
+    <MovieContainer>
+        <CoverImage src="https://images-na.ssl-images-amazon.com/images/I/71qXcEIdwbL.jpg" />
+        <MovieName> Movie Name </MovieName>
+    </MovieContainer>
+    );
+};
 
 export default MovieComponent;
