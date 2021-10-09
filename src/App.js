d716d98701fe8440da6bbe607ddc52ee30e7fe12
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-09 21:34:55 +0530

    Added Movie Info Component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 import styled from "styled-components";
 import MovieComponent from "./components/MovieComponent";
-const API_KEY = "f2499766";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+
+export const API_KEY = "a9118a3a";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
-
-const Header = styled.div`
+const AppName = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  align-items: center;
+`;
+const Header = styled.div`
   background-color: black;
   color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
   align-items: center;
   padding: 10px;
   font-size: 25px;
   font-weight: bold;
   box-shadow: 0 3px 6px 0 #555;
 `;
-
-const AppName = styled.div`
+const SearchBox = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
 `;
-
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
 const MovieImage = styled.img`
   width: 48px;
   height: 48px;
   margin: 15px;
 `;
-
-const SearchBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding: 10px 10px;
-  background-color: white;
-  border-radius: 6px;
-  margin-left: 20px;
-  width: 50%;
-  align-items: center;
-`;
-
-const SearchIcon = styled.img`
-  width: 32px;
-  height: 32px;
-`;
-
 const SearchInput = styled.input`
   color: black;
   font-size: 16px;
@@ -57,52 +52,73 @@ const SearchInput = styled.input`
   border: none;
   outline: none;
   margin-left: 15px;
-`
+`;
 const MovieListContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   padding: 30px;
-  justify-content: space-evenly;
-`
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
 
 function App() {
-  const [searchQuery, updateSearchQuery] = useState();
-  const [timeoutId, updateTimeoutId] = useState();
+  const [searchQuery, updateSearchQuery] = useState("");
+
   const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
 
-  const fetchData = async(searchString)=> {
-    const response = await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`);
-    console.log(response);
-    updateMovieList(response.data.Search)
-  }
+  const [timeoutId, updateTimeoutId] = useState();
 
-  const onTextChange = (event) => {
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
     clearTimeout(timeoutId);
-    updateSearchQuery(event.target.value);
-    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
     updateTimeoutId(timeout);
-  }
-
+  };
   return (
     <Container>
       <Header>
         <AppName>
           <MovieImage src="/movie-icon.svg" />
-           React Movie App 
+          React Movie App
         </AppName>
         <SearchBox>
-          <SearchIcon src="/search-icon.svg"/>
-          <SearchInput 
-            placeholder = "Search Movie"
-            value = {searchQuery}
-            onChange = {onTextChange}/>
+          <SearchIcon src="/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
         </SearchBox>
       </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
       <MovieListContainer>
-        {
-          movieList.length? movieList.map((movie, index)=> <MovieComponent/>):"No Movie search"
-        }
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/movie-icon.svg" />
+        )}
       </MovieListContainer>
     </Container>
   );
